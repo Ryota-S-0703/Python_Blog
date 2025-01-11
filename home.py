ace95eec077d7c85218020ad04e46c8add64f1c1
Ryota-S-0703 <shin.ryota.isid@gmail.com>
2025-01-11 13:23:05 +0900

    test4

--- home.py
+++ home.py
@@ -22,6 +22,7 @@ st.subheader("トレーニングデータ")
 st.dataframe(df)
 
 # データ入力
-min_date = datetime.date(1900, 1, 1)
-max_date = datetime.date(2100, 12, 31)
-d = st.date_input('誕生日を入力してください。', datetime.date(2025, 1, 11), min_value=min_date, max_value=max_date)
+min_date = datetime.date(1900, 1, 1)  # 選択可能な最小日付
+max_date = datetime.date(2100, 12, 31)  # 選択可能な最大日付
+default_date = datetime.date.today()  # 今日の日付をデフォルトに設定
+d = st.date_input('トレーニング実施日', default_date, min_value=min_date, max_value=max_date)
