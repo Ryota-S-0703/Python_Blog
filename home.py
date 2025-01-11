c44f35615ff9ff4fc0132eab593cd0be555a64f4
Ryota-S-0703 <shin.ryota.isid@gmail.com>
2025-01-11 13:30:38 +0900

    test5

--- home.py
+++ home.py
@@ -21,8 +21,18 @@ df = get_data_from_db()
 st.subheader("トレーニングデータ")
 st.dataframe(df)
 
+st.write("各項目について実施したものをチェックしてください。")
+
 # データ入力
 min_date = datetime.date(1900, 1, 1)  # 選択可能な最小日付
 max_date = datetime.date(2100, 12, 31)  # 選択可能な最大日付
 default_date = datetime.date.today()  # 今日の日付をデフォルトに設定
 d = st.date_input('トレーニング実施日', default_date, min_value=min_date, max_value=max_date)
+
+# チェックボックスの作成（デフォルトはオフ）
+core_training = st.checkbox("体幹トレーニング")
+peripheral_vision = st.checkbox("周辺視野トレーニング")
+indoor_handling = st.checkbox("室内ハンドリング")
+stretching = st.checkbox("ストレッチ")
+running = st.checkbox("ランニング")
+home_training = st.checkbox("自宅(20時まで家にいたか)")
