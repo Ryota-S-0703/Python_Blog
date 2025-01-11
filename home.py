ede1de591ebb735fd632971f6407f2e10e0e09cc
Ryota-S-0703 <shin.ryota.isid@gmail.com>
2025-01-11 13:17:47 +0900

    test3

--- home.py
+++ home.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import sqlite3
+import datetime
 
 # タイトルを表示
 st.title("トレーニングデータ管理アプリ")
@@ -19,3 +20,8 @@ df = get_data_from_db()
 # データを表示
 st.subheader("トレーニングデータ")
 st.dataframe(df)
+
+# データ入力
+min_date = datetime.date(1900, 1, 1)
+max_date = datetime.date(2100, 12, 31)
+d = st.date_input('誕生日を入力してください。', datetime.date(2025, 1, 11), min_value=min_date, max_value=max_date)
